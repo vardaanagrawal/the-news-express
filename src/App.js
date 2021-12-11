ad98ee50cb1b2d80b7942e75e12ceef157275c14
Vardaan <vardaanagrawal517@gmail.com>
2021-12-11 21:10:11 +0530

    bug fix

--- src/App.js
+++ src/App.js
@@ -4,21 +4,21 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
 import News from "./components/News";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 export default function App() {
   return (
     <div id="main_body">
       <Router>
         <Navbar />
 
-        <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/sports" element={<News category={"sports"} />} />
-          <Route path="/business" element={<News category={"business"} />} />
-          <Route path="/technology" element={<News category={"technology"} />} />
-          <Route path="/entertainment" element={<News category={"entertainment"} />} />
-          <Route path="/health" element={<News category={"health"} />} />
-        </Routes>
+        <Switch>
+          <Route path="/" exact><Home /></Route>
+          <Route path="/sports"><News category={"sports"} /></Route>
+          <Route path="/business"><News category={"business"} /></Route>
+          <Route path="/technology"><News category={"technology"} /></Route>
+          <Route path="/entertainment"><News category={"entertainment"} /></Route>
+          <Route path="/health"><News category={"health"} /></Route>
+        </Switch>
 
         <Footer />
       </Router>
